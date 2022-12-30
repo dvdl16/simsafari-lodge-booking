f26ef4f28a29a5c208344e0fe9b5e63aa3dfc200
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2022-12-30 18:39:16 +0200

    Fix iNaturalist API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://aiy3dcdty8.execute-api.eu-west-1.amazonaws.com/beta',
   authUrl: 'https://onsjabulani.auth.eu-west-1.amazoncognito.com/login?client_id=4k3dq5j2nouih7bdp7smnei8lf&response_type=token&scope=email+openid+profile&redirect_uri=https%3A%2F%2Fwww.onsjabulani.co.za/user/login',
-  iNatApiUrl: 'https://www.inaturalist.org/observations/dirkvdl16.json?layout=large&limit=10&order=desc&order_by=observed_on'
+  iNatApiUrl: 'https://www.inaturalist.org/observations/project/124844.json'
 };
