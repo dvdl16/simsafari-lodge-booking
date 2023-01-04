014054c3d2efaabd7a89d549e6723368c963ef45
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2023-01-04 13:39:25 +0200

    Add rates to houses

--- src/app/bookings/house.model.ts
+++ src/app/bookings/house.model.ts
@@ -3,14 +3,15 @@ export class House {
       public id: string,
       public name: string,
       public cssClass: string,
-      public allowBookings: boolean
+      public allowBookings: boolean,
+      public rate: number
     ) {}
 }
 
 export var Houses = [
-    new House("1", "Van der Laarse", "house1", false),
-    new House("2", "Small Guest House", "house2", true),
-    new House("3", "Lombards", "house3", false),
-    new House("4", "Rondavel", "house4", false),
-    new House("5", "Big Guest House", "house5", true),
+    new House("1", "Van der Laarse", "house1", false, 0),
+    new House("2", "Small Guest House", "house2", true, 100),
+    new House("3", "Lombards", "house3", false, 0),
+    new House("4", "Rondavel", "house4", false, 0),
+    new House("5", "Big Guest House", "house5", true, 125),
 ]
