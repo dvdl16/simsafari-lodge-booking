b8467208ff3a4db23e55a7aaa7da6be935e3560e
Dirk van der Laarse <dirk.van_der_laarse@partner.bmw.co.za>
2023-01-05 09:45:29 +0200

    Sightings: Improved layout and styling for mobile

--- src/app/sightings/sightings-list/sightings-list.component.ts
+++ src/app/sightings/sightings-list/sightings-list.component.ts
@@ -1,3 +1,4 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, Input, OnInit } from '@angular/core';
 import { Sighting } from '../sighting.model';
 
@@ -10,7 +11,43 @@ export class SightingsListComponent implements OnInit {
   @Input() loading!: boolean | null
   @Input() sightings!: Sighting[] | null
 
-  constructor() { }
+  public cols!: number;
+
+  gridByBreakpoint = {
+    xl: 4,
+    lg: 3,
+    md: 2,
+    sm: 2,
+    xs: 1
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).subscribe(result => {
+      if (result.matches) {
+        if (result.breakpoints[Breakpoints.XSmall]) {
+          this.cols = this.gridByBreakpoint.xs;
+        }
+        if (result.breakpoints[Breakpoints.Small]) {
+          this.cols = this.gridByBreakpoint.sm;
+        }
+        if (result.breakpoints[Breakpoints.Medium]) {
+          this.cols = this.gridByBreakpoint.md;
+        }
+        if (result.breakpoints[Breakpoints.Large]) {
+          this.cols = this.gridByBreakpoint.lg;
+        }
+        if (result.breakpoints[Breakpoints.XLarge]) {
+          this.cols = this.gridByBreakpoint.xl;
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
